fc6811c5747f9bad5a4e0a00e6d9b43051bfd34e
Goku-tech <gokulpc26102001@gmail.com>
2022-11-13 00:51:36 +0530

    Stage 26

---
+++ stage_26/stage26_compile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+./spl ../../os_lab/stage_26/os_startup/os_code.spl
+./spl ../../os_lab/stage_26/exception/exc_int.spl
+./spl ../../os_lab/stage_26/hardware_interrupts/console_interrupt.spl
+./spl ../../os_lab/stage_26/hardware_interrupts/disk_interrupt.spl
+./spl ../../os_lab/stage_26/hardware_interrupts/timer_interrupt.spl
+./spl ../../os_lab/stage_26/modules/resource_manager.spl
+./spl ../../os_lab/stage_26/modules/process_manager.spl
+./spl ../../os_lab/stage_26/modules/memory_manager.spl
+./spl ../../os_lab/stage_26/modules/file_manager.spl
+./spl ../../os_lab/stage_26/modules/device_manager.spl
+./spl ../../os_lab/stage_26/modules/scheduler.spl
+./spl ../../os_lab/stage_26/modules/boot_mod.spl
+./spl ../../os_lab/stage_26/software_interrupts/int_4.spl
+./spl ../../os_lab/stage_26/software_interrupts/int_5.spl
+./spl ../../os_lab/stage_26/software_interrupts/read_call.spl
+./spl ../../os_lab/stage_26/software_interrupts/write_call.spl
+./spl ../../os_lab/stage_26/software_interrupts/fork_call.spl
+./spl ../../os_lab/stage_26/software_interrupts/exec_call.spl
+./spl ../../os_lab/stage_26/software_interrupts/int_10.spl
+./spl ../../os_lab/stage_26/software_interrupts/int_11.spl
+./spl ../../os_lab/stage_26/software_interrupts/int_12.spl
+./spl ../../os_lab/stage_26/software_interrupts/int_13.spl
+./spl ../../os_lab/stage_26/software_interrupts/int_14.spl
+./spl ../../os_lab/stage_26/software_interrupts/shutdown_call.spl
+./spl ../../os_lab/stage_26/software_interrupts/int_16.spl
+./spl ../../os_lab/stage_26/software_interrupts/int_17.spl
