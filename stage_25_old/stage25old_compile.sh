2c872ff756a987a8cc6a7d5dce98d0652274ffbf
Goku-tech <gokulpc26102001@gmail.com>
2022-11-13 00:51:08 +0530

    Stage 25 working

---
+++ stage_25_old/stage25old_compile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+./spl ../../os_lab/stage_25_old/exception/exc_int.spl
+./spl ../../os_lab/stage_25_old/hardware_interrupts/console_interrupt.spl
+./spl ../../os_lab/stage_25_old/hardware_interrupts/disk_interrupt.spl
+./spl ../../os_lab/stage_25_old/hardware_interrupts/timer_interrupt.spl
+./spl ../../os_lab/stage_25_old/modules/boot_mod.spl
+./spl ../../os_lab/stage_25_old/modules/device_manager.spl
+./spl ../../os_lab/stage_25_old/modules/file_manager.spl
+./spl ../../os_lab/stage_25_old/modules/memory_manager.spl
+./spl ../../os_lab/stage_25_old/modules/process_manager.spl
+./spl ../../os_lab/stage_25_old/modules/resource_manager.spl
+./spl ../../os_lab/stage_25_old/modules/scheduler.spl
+./spl ../../os_lab/stage_25_old/os_startup/os_code.spl
+./spl ../../os_lab/stage_25_old/software_interrupts/exec_call.spl
+./spl ../../os_lab/stage_25_old/software_interrupts/fork_call.spl
+./spl ../../os_lab/stage_25_old/software_interrupts/int_4.spl
+./spl ../../os_lab/stage_25_old/software_interrupts/int_5.spl
+./spl ../../os_lab/stage_25_old/software_interrupts/int_10.spl
+./spl ../../os_lab/stage_25_old/software_interrupts/int_11.spl
+./spl ../../os_lab/stage_25_old/software_interrupts/int_13.spl
+./spl ../../os_lab/stage_25_old/software_interrupts/int_14.spl
+./spl ../../os_lab/stage_25_old/software_interrupts/read_call.spl
+./spl ../../os_lab/stage_25_old/software_interrupts/shutdown_call.spl
+./spl ../../os_lab/stage_25_old/software_interrupts/write_call.spl
